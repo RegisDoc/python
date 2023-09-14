3ed5e818b709b4b34f00f5da6b63f3f2a4e7f647
RegisDoc <regitahiri121@gmail.com>
2023-09-14 10:30:42 -0700

    first commit

---
+++ file.py
@@ -0,0 +1,83 @@
+# #coment ne python pr nje rresht
+# '''
+# komenti pr shm rreshta
+# '''
+# #deklarimi i variablave
+# #nk ka nevoje per fjale kyce
+# #nk duhet te perdoren fjale te rezervuara nga python
+# #emri i variablit duhet te nisi me germe; ose _
+
+# a=1
+# b=1
+# shuma=a+b
+# #afishimi i mesazhit
+# print(shuma)
+
+# # x=y=z=100.30
+# # print(x)
+# # print(y)
+# # print(z)
+
+# t,g,h=2, "test", '5.5'
+
+# #&&not aymre use--and; ||-> or, !-> not
+# # ==-> increment not anymore; ---> decrement not anymore
+
+# #afishimi
+# print('Hello Python')
+# #if string konvert also +str() if int convert the string to int
+# print('Shuma e dy numrave eshte '+str(shuma))
+# nr1 = input("Vendos nr e pare: ")
+# nr2 = input("Vendos nr e dyte: ")
+# print(nr1)
+# #print(nr2)
+# #print(int(nr1)+float(nr2))
+
+nr1=2
+nr2=3
+#conditions 
+'''
+if kushti :
+    instruksionet
+'''
+if nr1>nr2:
+    print(str(nr1)+ "me i madh se numri "+ str(nr2))
+
+'''
+if kushti :
+    instruksionet
+else:
+    instruksionet
+'''
+if nr1>nr2:
+    print(str(nr1)+ "me i madh se numri "+ str(nr2))
+else:
+    print(str(nr1)+ "me i vogel se numri"+ str(nr2))
+
+'''
+if kushti :
+    instruksionet
+elif kushti:
+    instruksionet    
+else:
+    instruksionet
+'''
+if nr1>nr2:
+    print(str(nr1)+ "me i madh se numri "+ str(nr2))
+elif nr1==nr2:
+    print(str(nr1)+ "i barabarte me "+ str(nr2))    
+else:
+    print(str(nr1)+ "me i vogel se numri"+ str(nr2))
+
+
+
+
+
+
+
+
+
+
+
+
+
